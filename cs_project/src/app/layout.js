b0ed75135a9648d8618f0177152ac8c05f9942da
Thanawat Kanphuton <framegg908@gmail.com>
2024-11-24 17:39:59 +0700

    "Update"

--- cs_project/src/app/layout.js
+++ cs_project/src/app/layout.js
@@ -1,13 +1,10 @@
-import { ThemeProvider } from '@/context/ThemeContext';
 import './globals.css';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <ThemeProvider>
           {children}
-        </ThemeProvider>
       </body>
     </html>
   );
